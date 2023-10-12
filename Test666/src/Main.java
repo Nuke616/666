1839956bd18cac34840bd77b147454d0b271d6b5
Никита <nuke581@gmain.com>
2023-10-12 20:13:15 +0300

    цикл + массив

--- Test666/src/Main.java
+++ Test666/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(444);
+        int[] people = new int[5];
+        int sum = 0;
+        for( int i = 0; i <people.length; i++){
+            people[i] = new Random().nextInt(100);
+            sum = sum + people[i];
+        }
+        System.out.println(sum);
+        System.out.println("Средний возраст:  " + sum / people.length);
+
     }
+
 }
+
+
